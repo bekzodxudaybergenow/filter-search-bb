aa2823436a0f8856f416d526af3b48d1e99bad22
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2024-12-03 23:47:29 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,87 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let getProducts = () => {
+//     products.forEach((item) => {
+//         let mainProducts = document.querySelector('.products-main');
+
+//         let card = document.createElement('div');
+//         let img = document.createElement('img');
+//         let content = document.createElement('div');
+//         let title = document.createElement('h1');
+//         let desc = document.createElement('p');
+//         let btn = document.createElement('button');
+
+//         img.src = item.image;
+//         img.alt = item.title;
+//         title.textContent = item.title;
+//         if(item.description.length > 150) {
+//             desc.textContent = item.description.slice(0, 150) + '...';
+//         }
+//         else {
+//             desc.textContent = item.description;
+//         }
+//         btn.textContent = 'Sotib olish'
+
+//         card.classList.add('card');
+//         img.classList.add('img');
+//         title.classList.add('title');
+//         desc.classList.add('desc');
+//         btn.classList.add('btn');
+    
+    
+    
+    
+//         mainProducts.appendChild(card);
+//         card.appendChild(img);
+//         card.appendChild(content);
+//         content.appendChild(title);
+//         content.appendChild(desc);
+//         content.appendChild(btn)
+//     })
+// }
+
+// getProducts();
+
